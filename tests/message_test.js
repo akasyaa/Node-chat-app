18f7f78b6ca58e31f9eca684adf26fcdb41a14a5
Sang Choi <akasyashc@gmail.com>
2017-01-27 18:20:32 -0800

    Add test case for location message

--- tests/message_test.js
+++ tests/message_test.js
@@ -1,13 +1,28 @@
 const { expect } = require('chai');
-const message = require('../server/utils/message');
+const { generateMessage, generateLocationMessage } = require('../server/utils/message');
 
 describe('genereateMessage', () => {
     it('should generate correct message object', () => {
         const from = 'Me';
         const text = 'Private Message';
-        const msg = message(from, text);
+        const msg = generateMessage(from, text);
 
         expect(msg.createdAt).to.be.a('number');
         expect(msg).to.include({ from, text });
     });
 });
+
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        const from = 'Me';
+        const lat = 123;
+        const lng = 456;
+        const msg = generateLocationMessage(from, lat, lng);
+
+        const url = `https://www.google.com/maps?q=${lat},${lng}`
+
+        expect(msg.createdAt).to.be.a('number');
+        expect(msg).to.include({ from, url });
+    })
+})
