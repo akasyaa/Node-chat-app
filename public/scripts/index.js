c5fbceb4b874a084850e38975b9a82cb163d0564
Sang Choi <akasyashc@gmail.com>
2017-01-29 20:47:22 -0800

    Timestamp messages using Moment.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -9,16 +9,18 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(msg) {
-    console.log('New message', msg);
-    var msg = $('<li></li>').text(`${msg.from}: ${msg.text}`);
+    var formattedTime = moment(msg.createdAt).format('h:mm a');
+    var msg = $('<li></li>').text(`${msg.from} ${formattedTime}: ${msg.text}`);
     $('#message-list').append(msg);
 });
 
 socket.on('newLocationMessage', function(msg) {
+    var formattedTime = moment(msg.createdAt).format('h:mm a');
+
     var li = $('<li></li>');
     var a = $('<a target="_blank">My current location</a>');
 
-    li.text(`${msg.from}: `);
+    li.text(`${msg.from} ${formattedTime}: `);
     a.attr('href', msg.url);
 
     li.append(a);
